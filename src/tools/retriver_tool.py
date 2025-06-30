7a652eada7fd3658e2b5ffcb8b65d5d503195801
ZAKI Hozaifa Ext O-EG/HRCS <rhlx8297@orange.com>
2025-06-30 20:25:39 +0300

    Updated Retriver Tool

--- src/tools/retriver_tool.py
+++ src/tools/retriver_tool.py
@@ -1,8 +1,7 @@
-from typing import Optional
+from pydantic import PrivateAttr, BaseModel
+from typing import Type
 
-from langchain_core.tools import BaseTool
-from langchain_core.tools.base import ArgsSchema
-from pydantic import PrivateAttr
+from crewai.tools import BaseTool
 
 from src.data_models.retriver_input import RetrieverInput
 
@@ -10,7 +9,7 @@ from src.data_models.retriver_input import RetrieverInput
 class RetrieverTool(BaseTool):
     name: str = "Movie Database Lookup"
     description: str = "Search for movies in the database based on titles or descriptions."
-    args_schema: Optional[ArgsSchema] = RetrieverInput
+    args_schema: Type[BaseModel] = RetrieverInput
     return_direct: bool = True
 
     _vector_store: object = PrivateAttr()
@@ -24,5 +23,4 @@ class RetrieverTool(BaseTool):
     def _run(self, query: str) -> str:
         """Synchronous search implementation."""
         results = self._vector_store.similarity_search(query, k=self._k)
-        print(results)
         return "\n".join(f"{i+1}. {doc.page_content}" for i, doc in enumerate(results))
